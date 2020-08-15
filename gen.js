6b5bed80dedc5524ce83cf57fe0f0e0aa493eed6
Danvass <68553241+danvogeko@users.noreply.github.com>
2020-08-15 12:41:17 -0400

    Update gen.js

--- gen.js
+++ gen.js
@@ -3,9 +3,9 @@
 	 charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
 	 
 	 function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min; 
+     	    min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min; 
     }
     
 
